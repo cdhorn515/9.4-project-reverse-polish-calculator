bb18b88e6ea8afecad54d7f39acc643df05907c6
Christina Cornell <cdhorn515@gmail.com>
2017-08-21 20:56:45 -0400

    more code cleanup

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,6 @@ public class Main {
         double actualResult = rpc.calculate(rpnExpression);
         checkResult(rpnExpression, expectedResult, actualResult);
 
-        // Write tests for the other operators (-, *, /)
-
-        // Uncomment this test which has many operators
         rpnExpression = "5,1,2,+,4,*,+,3,-";
         expectedResult = 14.0;
         actualResult = rpc.calculate(rpnExpression);
